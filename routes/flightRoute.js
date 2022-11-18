3b0ebc6c077a5c06069558280251495fceb1e147
moromoke01 <56760496+moromoke01@users.noreply.github.com>
2022-11-18 22:54:05 +0100

    Update flightRoute.js

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -1,16 +1,13 @@
-const router = require('express').Router()
-
-// const express = require('express');
-// const router = express.Router();
+const router = require('express').Router();
 const controller = require("../controllers/flightController")
 
-router.get("/", controller.getFlight)
-router.get("/:id", controller.getFlightEach)
-router.post("/", controller.CreateFlight)
-router.post("/:id", controller.updateFlightData)
-router.delete("/:id", controller.DeleteFlightRecord)
+router.get ("/", controller.getAllFlights);
+router.post ("/", controller.bookFlight);
+router.get("/:id", controller.getEachFlight);
+router.put("/:id", controller.updateFlight);
+router.delete("/:id", controller.removeFlight);
 
 
 
-module.exports =router;
 
+module.exports = router;
