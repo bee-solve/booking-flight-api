0847ae4188f467bcdcb23ae79bff120ce2aac92d
moromoke01 <56760496+moromoke01@users.noreply.github.com>
2022-11-18 22:56:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+
 const express = require("express");
-const { json } = require("express");
+const {v4: uuid} = require('uuid');
+const { json }= require("express");
 
 const flight = require('./routes/flightRoute.js');
 
 const app = express();
 app.use(json());
 
+app.use("/Flight", flight);
 
-app.use("/flights", flight)
 
-app.get("/", (req, res) =>{
-  res.send("Jane is building flight App");
+
+
+app.get("/", (req, res)=>{
+    res.send("working on a project");
+ 
 })
-
-
 const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT,()=>console.log(`serving on port ${PORT}`));
